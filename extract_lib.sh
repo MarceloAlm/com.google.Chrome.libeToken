393ddb4252bbac1e8fbb3cb58743ed93171e66da
Marcelo de Almeida <marceloalm@outlook.com.br>
2025-03-06 15:53:54 -0300

    Only install proposed library and create libeToken.module for the Flatpak Chrome.

--- extract_lib.sh
+++ extract_lib.sh
@@ -4,4 +4,9 @@ set -e
 bsdtar -Oxf safenetauthenticationclient-core_10.8.1050_amd64.deb data.tar.gz |
   bsdtar -xf - --strip-components=2 ./usr/lib
 
-cp -r lib ${FLATPAK_DEST}/
+mkdir -p ${FLATPAK_DEST}/lib/pkcs11
+install lib/libeToken.so.10.8.1050 ${FLATPAK_DEST}/lib/
+ln -s ${FLATPAK_DEST}/lib/libeToken.so.10.8.1050 ${FLATPAK_DEST}/lib/pkcs11/libeToken.so
+
+mkdir -p ${FLATPAK_DEST}/modules
+echo "module:${FLATPAK_DEST}/lib/pkcs11/libeToken.so" > ${FLATPAK_DEST}/modules/libeToken.module
--- modutil.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if ! modutil -dbdir sql:~/.pki/nssdb -list 2>/dev/null | grep -q "eToken"; then
-  echo "Adicionando o módulo 'eToken' ao banco de dados NSS..."
-  modutil -dbdir sql:~/.pki/nssdb -add "eToken" -libfile ${FLATPAK_DEST}/lib/libeToken.so -force
-fi
