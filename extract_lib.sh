218a9c8db110e2af771d119936efdcdbf8a6f5cb
Marcelo de Almeida <marceloalm@outlook.com.br>
2025-03-07 14:45:51 -0300

    change the source of libs to extra-data added Thales Copyright

--- extract_lib.sh
+++ extract_lib.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/sh
 set -e
 
-bsdtar -Oxf safenetauthenticationclient-core_10.8.1050_amd64.deb data.tar.gz |
+echo "*** Extract files"
+unzip -p safenet.zip Ubuntu-2204/safenetauthenticationclient_10.8.1050_amd64.deb |
+  bsdtar -Oxf - data.tar.gz |
   bsdtar -xf - --strip-components=2 ./usr/lib
 
+echo "*** Install ${FLATPAK_DEST}/lib/pkcs11/libeToken.so"
 mkdir -p ${FLATPAK_DEST}/lib/pkcs11
 install lib/libeToken.so.10.8.1050 ${FLATPAK_DEST}/lib/
 ln -s ${FLATPAK_DEST}/lib/libeToken.so.10.8.1050 ${FLATPAK_DEST}/lib/pkcs11/libeToken.so
 
+echo "*** Create ${FLATPAK_DEST}/modules/libeToken.module"
 mkdir -p ${FLATPAK_DEST}/modules
 echo "module:${FLATPAK_DEST}/lib/pkcs11/libeToken.so" > ${FLATPAK_DEST}/modules/libeToken.module
